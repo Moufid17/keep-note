e3df14bcffdfe42464d1c3b1080c13d91c95aacd
Moufid17 <moufid.mtr@gmail.com>
2025-05-03 14:46:14 +0200

    feat: implement user authentication functions and database schema for notes and users (prisma && supabase)

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type ErrorResponse = {
+  errorMessage: string
+}
+
+export const handleError = (error: unknown) : ErrorResponse => {
+  if (error instanceof Error) {
+    return {errorMessage: error.message}
+  }
+  return {errorMessage: "An unknown error occurred."}
+}
