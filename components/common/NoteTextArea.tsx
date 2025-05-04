4b30295f24a67ddd2d48852949e8be8b89f20a1b
Moufid17 <moufid.mtr@gmail.com>
2025-05-04 03:59:36 +0200

    feat: (init NoteTextArea)integrate useNote hook for managing note text state in NoteTextArea component

--- components/common/NoteTextArea.tsx
+++ components/common/NoteTextArea.tsx
@@ -1,9 +1,23 @@
+"use client"
 import React from 'react'
 import { Textarea } from '@/components/ui/textarea'
+import useNote from '@/hooks/useNote';
 
 function NoteTextArea() {
+  const {noteText, setNoteText} = useNote();
+
+  const handleOnchangeNoteTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNoteText(e.target.value)
+  }
+
   return (
-    <Textarea placeholder="Type your message here..." className="w-full max-w-4xl min-h-124"/>
+    <Textarea
+      value={noteText}
+      onChange={handleOnchangeNoteTextArea}
+      autoFocus
+      placeholder="Type your message here..." 
+      className="w-full max-w-4xl min-h-124"
+    />
   )
 }
 
