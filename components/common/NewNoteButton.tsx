a5a569c14adf4844f71e2a8e5fa06c18b75822c9
Moufid17 <moufid.mtr@gmail.com>
2025-05-06 02:01:11 +0200

    clean

--- components/common/NewNoteButton.tsx
+++ components/common/NewNoteButton.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { User } from "@supabase/supabase-js";
 import { useRouter } from 'next/navigation';
@@ -12,7 +12,7 @@ type NewNoteButtonProps = {
 
 function NewNoteButton({ user }: NewNoteButtonProps) {
     const router = useRouter();
-    const [isLoading, setIsLoading] = React.useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     
     const handleClickNewNoteButton = async () => {
         if (!user) router.push("/login");
@@ -25,8 +25,8 @@ function NewNoteButton({ user }: NewNoteButtonProps) {
             if (error.errorMessage) {
                 toast.error(error?.errorMessage);
             } else {
-                toast.success("Note created successfully");
                 router.push(`/?noteId=${uuid}`);
+                toast.success("Note created successfully");
             }
         }
         
