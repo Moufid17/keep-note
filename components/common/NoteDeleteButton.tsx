1695007c428045dc462ac74674cbebbc7907493b
Moufid17 <moufid.mtr@gmail.com>
2025-06-06 18:29:31 +0200

    feat: refactor NoteDeleteButton to streamline archiving functionality and update button labels

--- components/common/NoteDeleteButton.tsx
+++ components/common/NoteDeleteButton.tsx
@@ -1,8 +1,7 @@
 "use client"
-import { useEffect, useState, useTransition } from 'react'
+import { useTransition } from 'react'
 import { Trash2 } from 'lucide-react'
 import { toast } from "sonner"
-import { updateNoteArchiveAction } from '@/app/actions/notes'
 import {
     AlertDialog,
     AlertDialogAction,
@@ -14,7 +13,8 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button'
+import { updateNoteArchiveAction } from '@/app/actions/notes'
   
 
 interface INoteDeleteButton {
@@ -24,32 +24,25 @@ interface INoteDeleteButton {
 
 function NoteDeleteButton(props: INoteDeleteButton) {
     const { noteId, onDeleteLocally } = props
-    const [localNoteId, setLocalNoteId] = useState<string>(noteId)
     const [isPendingToArchiveNote, startTransitionToArchiveNote] = useTransition()
 
-    useEffect(() => {
-        if (noteId) {
-            setLocalNoteId(noteId)
-        }
-    }, [noteId])
-
     const handleArchiveNote = () => {
-        startTransitionToArchiveNote(async() => {
-            const error = await updateNoteArchiveAction(localNoteId, true)
+        startTransitionToArchiveNote(async() => {   
+            const error = await updateNoteArchiveAction(noteId, true)
             if (error?.errorMessage) {
                 toast.error(error.errorMessage)
             } else {
+                onDeleteLocally && onDeleteLocally()
                 toast.success("Note archived successfully")
             }
         })
-        if (!isPendingToArchiveNote && onDeleteLocally) onDeleteLocally()
     }
 
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <Button variant="ghost" className="cursor-pointer w-full text-brand-500 hover:text-brand-500 ">
-                    <Trash2 className="py-0"/><span className='text-left'>Delete</span>
+                <Button variant="ghost" className="cursor-pointer w-full text-brand-500 hover:text-brand-500">
+                    <Trash2 className="py-0"/><span className='text-left'>Archive</span>
                 </Button>
             </AlertDialogTrigger>
             <AlertDialogContent>
@@ -62,7 +55,7 @@ function NoteDeleteButton(props: INoteDeleteButton) {
                 <AlertDialogFooter>
                 <AlertDialogCancel>Ignore</AlertDialogCancel>
                 <AlertDialogAction onClick={handleArchiveNote} disabled={isPendingToArchiveNote} className='bg-brand-500 text-black hover:bg-brand-500 hover:text-black'>
-                    <span className="text-black">Delete</span>
+                    <span className="text-black">Archive</span>
                 </AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
