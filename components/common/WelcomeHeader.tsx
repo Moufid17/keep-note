fd269a8d9ea6047d59063ccd9ff0f41d39751853
Moufid17 <moufid.mtr@gmail.com>
2025-06-10 15:52:02 +0200

    feat: - create WelcomeHeader component for user navigation and branding - refactor Header component structure for improved layout and readability

--- components/common/WelcomeHeader.tsx
+++ components/common/WelcomeHeader.tsx
@@ -4,27 +4,32 @@ import { Button } from "../ui/button";
 import { ModeToggle } from "../common/DarkModeToggle";
 import LogoutButton from "../common/LogoutButton";
 import { getUser } from "@/auth/server";
-import { SidebarTrigger } from "../ui/sidebar";
 
-export default async function Header() {
+async function WelcomeHeader() {
   const user = await getUser();
   return (
-    <header className={`relative flex justify-center h-24 w-full`}>
-      <div className="absolute flex h-20 mt-4 w-full items-center justify-between px-3 sm:px-6 rounded-2xl border gap-2">
+    <header className="relative  flex justify-center h-24 w-full mx-auto">
+      <div className="absolute z-3 flex h-20 mt-4 w-[90%] items-center justify-between px-3 sm:px-6 rounded-2xl border bg-background shadow-md transition-all duration-300 ease-in-out hover:shadow-lg">
         <div className="flex items-center gap-4">
-          {user && (<div><SidebarTrigger className="h-full"/></div>)}
           <div>
             <Link href={"/"} className="flex items-center gap-2">
-            <Image src={"/app.svg"} width={60} height={60} alt="logo" priority className="rounded-full" />
-            <h1 className="hidden md:visible md:flex flex-col pb-1 text-2xl font-semibold leading-6">
-              Keep <span>Notes</span>
-            </h1>
-          </Link>
+              <Image src={"/app.svg"} width={60} height={60} alt="logo" priority className="rounded-full" />
+              <h1 className="hidden md:visible md:flex flex-col pb-1 text-2xl font-semibold leading-6">
+                Keep <span>Notes</span>
+              </h1>
+            </Link>
           </div>
         </div>
         <div className="flex gap-4">
           {user ? (
-            <LogoutButton />
+            <>
+              <Button asChild>
+                <Link href={"/notes"} >
+                  Dashboard
+                </Link>
+              </Button>
+              <LogoutButton />
+            </>
           ) : (
                 <>
                   <Button asChild className="sm:block">
@@ -45,3 +50,5 @@ export default async function Header() {
     </header>
   )
 }
+
+export default WelcomeHeader
