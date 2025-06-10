8aa41812b3442b858d354fcdadfcad09e7f1cf94
Moufid17 <moufid.mtr@gmail.com>
2025-06-10 21:46:48 +0200

    feat: - Implement Middleware - implement note creation and retrieval API endpoints

---
+++ app/api/fetch-newest-note/route.ts
@@ -0,0 +1,23 @@
+import { prismaClient } from "@/db/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const userEmail = searchParams.get("email") || "";
+
+  const newestNoteId = await prismaClient.note.findFirst({
+    where: {
+        author:{email: userEmail},
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return NextResponse.json({
+    newestNoteId: newestNoteId?.id,
+  });
+}
