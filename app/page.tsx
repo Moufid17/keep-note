4c1bb61461caacf1a6f1408ef72e88e1492ee299
Moufid17 <moufid.mtr@gmail.com>
2025-06-10 15:50:27 +0200

    feat:  - refactor layout and remove unused components for improved structure and UI - create WelcomePage component with animated text and navigation buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,5 @@
-import AskAIMenu from "@/components/common/AskAIMenu";
-import HomePage from "@/components/common/HomePage";
-import NewNoteButton from "@/components/common/NewNoteButton";
-import NoteTextArea from "@/components/common/NoteTextArea";
-import { generateNoteId } from "@/lib/utils";
-import { prismaClient } from "@/db/prisma";
-import { getUser } from "@/auth/server";
+import WelcomePage from "@/components/common/WelcomePage";
 
-
-type Props = {
-  searchParams: Promise<{
-    [key: string]: string | string[] | undefined;
-  }>;
-};
-
-export default async function Home({ searchParams }: Props) {
-  const user = await getUser();
-
-  if (!user) return ( <HomePage /> )
-    
-  const nodeIdParam = (await searchParams).noteId || "";
-  
-  let noteId = (Array.isArray(nodeIdParam)) ? nodeIdParam[0] : nodeIdParam;
-  if (noteId.length === 0) noteId = generateNoteId()
-
-  const note = await prismaClient.note.findUnique({
-    where: {
-      id: noteId, 
-      author: { email: user?.email }  
-    },
-    select: {
-      text: true,
-    }
-  });
-
-  return (
-    <div className="flex flex-col items-center w-full h-[85vh]">
-      <div className="w-full max-w-4xl flex flex-col justify-center gap-4 p-2">
-        <div className="flex gap-4 justify-end items-center">
-          <AskAIMenu />
-          <NewNoteButton user={user}/>
-        </div>
-        <NoteTextArea noteId={noteId} startingNoteText={note ? note.text : ""}/>       
-      </div>
-    </div>
-  );
+export default function Page() {
+  return ( <WelcomePage /> );
 }
