0ec4ba59da82fac1065c09d8581c049db0744aa4
Moufid17 <moufid.mtr@gmail.com>
2025-06-13 21:05:55 +0200

    fix: restore original environment variable names for Supabase client initialization

--- auth/client.ts
+++ auth/client.ts
@@ -6,10 +6,7 @@ import { createBrowserClient } from '@supabase/ssr'
 */
 // This function retreive user
 export async function getClientUser() {
-    // if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-    //     throw new Error('Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables');
-    // }
-    const client = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+    const client = createBrowserClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
     const {
         data: { user }, error
     } = await client.auth.getUser()
