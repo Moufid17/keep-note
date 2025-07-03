773aeb5bc5c76196f56931cad49cde9e4b9efc9e
Moufid17 <moufid.mtr@gmail.com>
2025-07-03 12:11:17 +0200

    feat: update Note model and migrations to support optional tag association and add title and isArchived fields

--- db/migrations/20250506002329_add_note_title_and_archive/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Note" ADD COLUMN     "isArchived" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "title" TEXT NOT NULL DEFAULT '';
--- db/migrations/20250702180935_note_and_tag/migration.sql
+++ db/migrations/20250702180935_note_and_tag/migration.sql
@@ -1,10 +1,13 @@
 -- CreateTable
 CREATE TABLE "Note" (
     "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL DEFAULT '',
     "text" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
+    "tagId" TEXT NOT NULL DEFAULT '',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isArchived" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
 );
@@ -19,8 +22,26 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
+
 -- AddForeignKey
 ALTER TABLE "Note" ADD CONSTRAINT "Note_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
