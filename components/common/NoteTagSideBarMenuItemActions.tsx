969a6d0d5becdf102f80b69fc5bf35d29e762bf7
Moufid17 <moufid.mtr@gmail.com>
2025-07-07 23:16:32 +0200

    fix: refactor tag color handling and improve dropdown state management

--- components/common/NoteTagSideBarMenuItemActions.tsx
+++ components/common/NoteTagSideBarMenuItemActions.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { toast } from "sonner"
 import { SidebarMenuAction, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { EllipsisVertical, SquarePen, Trash2, XIcon } from "lucide-react"
@@ -18,7 +18,7 @@ import { NoteType } from "@/types/notes"
 
 const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagType, onRemoveLocally: (value:string) => void}) => {
     const router = useRouter()
-    const { id, name, color } = data
+    const { id, name, color: initialColor } = data
     
     const noteId = useSearchParams().get("noteid") ?? ""
     const tagIdParam = useSearchParams().get("tagId") ?? ""
@@ -26,11 +26,11 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
 
     const [localTagId, setLocalTagId] = useState<string>(id)
     const [localTagName, setLocalTagName] = useState<string>(name)
-    const [localTagColor, setTagColor] = useState<string>(color)
+    const [localTagColor, setTagColor] = useState<string>(initialColor)
 
     const [isPendingToUpdateTagName, startTransitionToUpdateTagName] = useTransition()
     const [isLoading, setIsLoading] = useState(false)
-    const [isOpenDropdown, setIsOpenDropdown] = useState(false)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
     const {updateItem: updateTag } = useTagStore((state) => state)
 
@@ -47,16 +47,10 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
         if (!isPendingToUpdateTagName) {
             setIsLoading(false)
         }
-        setIsOpenDropdown(false)
     }
 
     const handleChangeTagColor = async (color: string) => {
         setTagColor(color)
-        await updateTag(localTagId, { name: localTagName.trim().toLowerCase(), color: color.trim()})
-        toast.success("Tag", {
-            position: "top-right",
-            description: "Tag color changed successfully"
-        })
     }
 
     const handleChangeTag = async () => {
@@ -84,8 +78,21 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
         setLocalTagId("")
         setLocalTagName("")
         setTagColor("")
-        setIsOpenDropdown(false)
     }
+    const handleChangeTagColorWhenDropDownMenuIsClosedAndInitialColorChanged = async () => {
+        if (!isDropdownOpen && localTagColor !== initialColor) {
+            // l'utilisateur a fermé le menu, on applique les changements
+            
+            await updateTag(localTagId, { name: localTagName.trim().toLowerCase(), color: localTagColor.trim()})
+            toast.success("Tag", {
+                position: "top-right",
+                description: "Tag color changed successfully"
+            })
+        }
+    }
+    useEffect(() => {
+        handleChangeTagColorWhenDropDownMenuIsClosedAndInitialColorChanged()
+    }, [isDropdownOpen, handleChangeTagColorWhenDropDownMenuIsClosedAndInitialColorChanged])
 
     if (isLoading) {
         return (
@@ -106,7 +113,7 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
                     className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
                     onClick={() => {
                         setLocalTagName(name)
-                        setTagColor(color)
+                        setTagColor(initialColor)
                         setIsLoading(false)
                         setLocalTagId(id)
                     }}
@@ -122,18 +129,13 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
             <SidebarMenuButton asChild className={`cursor-pointer mb-1`} onClick={handleChangeTag}>
                 <div><TagIcon color={localTagColor}/><span className="text-sm px-2">{UpperCaseFirstLetter(localTagName)}</span></div>
             </SidebarMenuButton>
-            <DropdownMenu open={isOpenDropdown} onOpenChange={setIsOpenDropdown}>
+            <DropdownMenu onOpenChange={setIsDropdownOpen}>
                 <DropdownMenuTrigger asChild>
                     <SidebarMenuAction>
                         <EllipsisVertical className="text-black" />
                     </SidebarMenuAction>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent side="bottom" align="start">
-                    <DropdownMenuItem className="cursor-pointer" 
-                        onClick={() => {setIsLoading(true); setLocalTagId(id);}}
-                    >
-                        <SquarePen /><span>Rename</span>
-                    </DropdownMenuItem>
                     <DropdownMenuGroup className="cursor-pointer justify-center">
                         <DropdownMenuSub>
                             <DropdownMenuSubTrigger className='cursor-pointer text-center"'>Tag Color</DropdownMenuSubTrigger>
@@ -146,6 +148,11 @@ const NoteTagSideBarMenuItemActions = ({data, onRemoveLocally}:{data: NoteTagTyp
                             </DropdownMenuPortal> 
                         </DropdownMenuSub>
                     </DropdownMenuGroup>
+                    <DropdownMenuItem className="cursor-pointer" 
+                        onClick={() => {setIsLoading(true); setLocalTagId(id);}}
+                    >
+                        <SquarePen /><span>Rename</span>
+                    </DropdownMenuItem>
                     <DropdownMenuItem asChild>
                         <Button variant="ghost" className="cursor-pointer w-full justify-start text-red-500 hover:text-red-500" 
                             onClick={() => {
